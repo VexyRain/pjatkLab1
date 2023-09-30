b6e7a9c29c5bab71cd9f340262e4a87f67348365
Jakub Strzałkowski <s31893@pjwstk.edu.pl>
2023-09-30 15:54:20 +0200

    Initial commit z kodem

---
+++ Nowy Folder.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+/* Ten program wyœwietla pozytywne wartoœci powy¿ej 0, które mo¿e posiadaæ
+zmienna typu int, w kolejnoœci od najmniejszej do najmniejszej, bez ¿adnego
+formatowania.
+*/
+
+
+int main()
+{
+    int x = 0;
+    while (true) {
+        x = x + 1;
+        cout << x;
+    };
+
+}
